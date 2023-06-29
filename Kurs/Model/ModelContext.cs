383362dc81bab2254e10631b6af7bf9b5176c9e7
elik <pochta@bk.ru>
2023-06-29 12:33:54 +0200

    structure project and log

---
+++ Kurs/Model/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Kurs.Model
+{
+    public class ModelContext : DbContext
+    {
+        public DbSet<User> User { get; set; } = null;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Duma.db");
+        }
+    }
+}
---
+++ Kurs/View/LoginView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Kurs.ViewModel;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kurs.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel(this);
+        }
+    }
+}
